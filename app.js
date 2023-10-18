a1b8515131eddece90c5535fce9cda18664e75de
Ballzpoppy <147939667+Ballzpoppy@users.noreply.github.com>
2023-10-18 17:33:14 +0100

    Update app.js

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 /*********************Route Query Start*********************/
